ac87cbe8fd144e35599768d61265a4fef5350a3c
atrapp <andrea.trapp@gmx.net>
2014-07-31 12:15:38 -0400

    change to no compression of javascript

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.assets.precompile = []
+
+  config.assets.compress = true 
+  config.assets.js_compressor = NoCompression.new
 end
